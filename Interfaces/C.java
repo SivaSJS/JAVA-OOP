770f5180d8e94db72527f53074eeaa9dd080f411
Siva S <68813056+SivaSJS@users.noreply.github.com>
2022-02-26 21:31:00 +0530

    Add files via upload

---
+++ Interfaces/C.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class C implements A{
+
+
+    @Override
+    public void value() {
+        // similar like abstract it also needs to override whats inside in interfaces
+    }
+}
