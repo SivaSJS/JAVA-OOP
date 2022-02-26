815a3f68197552c67fbb57bffc9e6fdf42402f4a
Siva S <68813056+SivaSJS@users.noreply.github.com>
2022-02-26 21:17:37 +0530

    Add files via upload

---
+++ Abstract class/A.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public abstract class A {
+    public static int val; // we could declare static variable without giving any value
+
+    abstract void ret();
+
+
+}
+
+
---
+++ Abstract class/C.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class C  extends A{
+
+
+    @Override
+    void ret() {
+          // since we extends class A which is abstract
+        // we need to override whats inside in abstract class .
+    }
+}
